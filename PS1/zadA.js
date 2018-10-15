1667a6573cd8b1d09a899411222896835d2a30fe
MathMatowicki <matowicki@gmail.com>
2018-10-15 16:06:44 +0200

    Add first alghoritm to exercise A.

--- PS1/zadA.js
+++ PS1/zadA.js
@@ -1,3 +1,17 @@
-function newtonAlgorithmIteration(n, m) {}
+function newtonAlgorithmIteration(n, m) {
+    let newton = 0;
+    for(let i = 0; i < m; i++){
+        newton =newton*(n-i+1)/i;
+    }
+    return newton;
+}
 
-function newtonAlgorithmRecurssion(n, m) {}
+function newtonAlgorithmRecursion(n, m) {}
+
+console.log(newtonAlgorithmIteration(2,1));
+console.log(newtonAlgorithmIteration(2,1));
+console.log(newtonAlgorithmIteration(2,1));
+console.log(newtonAlgorithmIteration(2,1));
+console.log(newtonAlgorithmIteration(2,1));
+console.log(newtonAlgorithmIteration(2,1));
+
