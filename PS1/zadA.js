eab76625673871b34b80c446785cc6e08ad54cf2
MathMatowicki <matowicki@gmail.com>
2018-10-17 08:03:53 +0200

    Add factorial function to exerciseA. And file reading in Exercise2.js

--- PS1/zadA.js
+++ PS1/zadA.js
@@ -1,10 +1,22 @@
 function newtonAlgorithmIteration(n, m) {
-    let out =1;
-    for(let i = 1; i <= m; i++){
-        out *=(n-i+1)/i;
+    let out = 1;
+    for (let i = 1; i <= m; i++) {
+        out *= (n - i + 1) / i;
     }
     return out
 }
+let array = [],
+    factorial = 1;
 
-console.log(newtonAlgorithmIteration(2,1));
-console.log(newtonAlgorithmRecursion(2,1));
+array.push(factorial);
+
+function factorialOfNumber(number) {
+    for (let i = 1; i < number; i++) {
+        factorial = array[i - 1] * i;
+        array.push(factorial);
+    }
+    return array
+}
+console.table(factorialOfNumber(25));
+console.log(newtonAlgorithmIteration(2, 1));
+console.log(newtonAlgorithmRecursion(2, 1));
--- PS2/Exercise2.js
+++ PS2/Exercise2.js
@@ -1,14 +1,44 @@
-let array = [], factorial = 1;
-
-array.push(factorial);
-
-
-function factorialOfNumber(number){
-    for (let i = 1; i < number; i++) {
-        factorial = array[i-1] *i;
-        array.push(factorial); 
-    }
-    return array
+function readFromTxt() {
+    document.getElementById('openFile').addEventListener('change', function () {
+        const fr = new FileReader();
+        fr.onload = function () {
+            document.getElementById('fileContents').textContent = this.result;
+        }
+        fr.readAsText(this.files[0]);
+    });
 }
 
-console.table(factorialOfNumber(25));
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function algorithmA(array) {
+
+
+    return
+}
+
+
+
+let inData = [
+    [5],
+    [7, 2, 11, 4],
+    [7, 1, 9, 2],
+    [4, 2, 6, 4],
+    [1, 1, 2, 2],
+    [2, 1, 5, 6],
+    [3]
+];
+
+
+console.table(inData);
+
+for (let i = getRandomInt(9999, 10000); i > 0; i--) {
+    inData.push(getRandomInt(1, 10000));
+}
